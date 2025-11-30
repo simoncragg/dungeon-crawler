26d204e41c59b4ac1d47d0f67b4a5115b839db4d
Simon Cragg <22366916+simoncragg@users.noreply.github.com>
2025-11-30 20:12:28 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+
+  return (
+    <>
+      <header className="flex justify-center items-center p-2 bg-slate-900 border-b border-slate-800 shadow-md z-10 shrink-0">
+        <div className="flex items-center gap-2 text-emerald-400 text-xl">
+          <span className="font-serif mt-1 font-bold tracking-widest">
+            DUNGEON CRAWLER
+          </span>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default App
