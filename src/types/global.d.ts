2879ed29f05d5d35ce47ac0ff22e4b5d7853ef68
Simon Cragg <22366916+simoncragg@users.noreply.github.com>
2025-12-05 17:42:20 +0000

    refactor: set indentation to 2 spaces

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,7 +1,7 @@
 export { };
 
 declare global {
-    interface Window {
-        webkitAudioContext: typeof AudioContext;
-    }
+  interface Window {
+    webkitAudioContext: typeof AudioContext;
+  }
 }
