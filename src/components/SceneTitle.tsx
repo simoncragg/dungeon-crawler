356b5c6b640bda28e0daf169a745fc30d2d48ec4
Simon Cragg <22366916+simoncragg@users.noreply.github.com>
2025-12-25 21:47:04 +0000

    feat: reduce scene title fade out time

--- src/components/SceneTitle.tsx
+++ src/components/SceneTitle.tsx
@@ -28,7 +28,7 @@ export default function SceneTitle({ title, forceHide }: SceneTitleProps) {
     const show = isVisible && !forceHide;
 
     return (
-        <div className={`transition-opacity ${forceHide ? "duration-0" : "duration-1000"} ${show ? "opacity-100" : "opacity-0"}`}>
+        <div className={`transition-opacity ${forceHide ? "duration-0" : "duration-500"} ${show ? "opacity-100" : "opacity-0"}`}>
             <h2 className="text-3xl font-medieval text-emerald-500 tracking-widest uppercase drop-shadow-[0_1px_2px_rgba(0,0,0,1)] text-center">
                 {title}
             </h2>
