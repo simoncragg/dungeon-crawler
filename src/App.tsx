53349eaaf89d030fa674c74d8942b0ef8f30d216
Simon Cragg <22366916+simoncragg@users.noreply.github.com>
2025-12-26 02:40:15 +0000

    feat: add hard reset to header title

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,12 @@ const App = () => {
       {hasStarted && (
         <>
           <header className="flex justify-center items-center p-2 bg-slate-900 border-b border-slate-800 shadow-md z-10 shrink-0">
-            <div className="flex items-center gap-2 text-emerald-400 text-xl">
+            <div
+              className="flex items-center gap-2 text-emerald-400 text-xl cursor-pointer hover:text-emerald-300 transition-colors"
+              onClick={() => window.location.reload()}
+              role="button"
+              title="Reload Game"
+            >
               <span className="font-medieval mt-1 font-bold tracking-widest">
                 DUNGEON CRAWLER
               </span>
